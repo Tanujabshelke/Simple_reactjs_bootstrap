06106b8751e03dd69750e425bdd1d26c25c999a8
Tanuja <tanujabshelke@gmail.com>
2019-09-22 12:31:59 +0530

    Updates

---
+++ precache-manifest.9185c44fdf4bda77bfad00f2d5100ce0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77f7657746a651e4f24142ed1503b7bb",
+    "url": "/Simple_reactjs_bootstrap/index.html"
+  },
+  {
+    "revision": "e9c87b858fe4c5f06eed",
+    "url": "/Simple_reactjs_bootstrap/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "f569590dfd25427607af",
+    "url": "/Simple_reactjs_bootstrap/static/css/main.5f49a9b0.chunk.css"
+  },
+  {
+    "revision": "e9c87b858fe4c5f06eed",
+    "url": "/Simple_reactjs_bootstrap/static/js/2.1c1a0438.chunk.js"
+  },
+  {
+    "revision": "f569590dfd25427607af",
+    "url": "/Simple_reactjs_bootstrap/static/js/main.7583a2f7.chunk.js"
+  },
+  {
+    "revision": "e99ca7c61d18f5c4cc58",
+    "url": "/Simple_reactjs_bootstrap/static/js/runtime~main.3c264018.js"
+  }
+]);
