513cc123884e772b94150fdf3c6c34f1b2d7efa5
Tanuja <40301041+Tanujabshelke@users.noreply.github.com>
2019-10-11 00:45:14 +0530

    Update App_1.js

--- src/App_1.js
+++ src/App_1.js
@@ -2,19 +2,17 @@ import React from 'react';
 import Header from './Header'
 import Main from './Main'
 import Footer from './Footer'
-import Info from './Data'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
   
 class App_1 extends React.Component {
    
     render(){
-        const Info_1 = Info.map(data => <Main key={data.id} id={data.id} title={data.title} 
-                        subtitle={data.subtitle} img={data.img} benefit={data.benefit} /> )
+        
         return(
             <div>
                 <Header />
-                { Info_1}
+                <Main />
                 <Footer />
                
                 
