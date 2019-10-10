0bfeeaedb82328b4a32c36c6e0ed7f945aa192cf
Tanuja <40301041+Tanujabshelke@users.noreply.github.com>
2019-10-11 00:42:49 +0530

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -1,27 +1,15 @@
-import React from 'react';
-import {Navbar} from 'react-bootstrap';
-function Header(){
-    return(
-        <div className="tcolor">
-            <Navbar bg="dark" variant="dark" >
-                <Navbar.Brand href="#home">
-                    <img
-                        alt="logo"
-                        src="./img/logo.jpg"
-                        width="30"
-                        height="30"
-                        className="d-inline-block align-top"
-                    />
-                    
-                    {  ' Stay fit & calm with Yoga' }
-                
-               
-                </Navbar.Brand>
-            </Navbar>
-        </div>
-            
-            
-        
+import React from "react"
+import './index.css'
+
+function Header() {
+    return (
+        <header >
+            <img  style={{height :"80%",marginLeft:"10%", padding:"2%"}}
+                src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" 
+                alt="Problem?"
+            />
+            <h1 className="text-white text-center p-4">Meme Generator</h1>
+        </header>
     )
 }
 
