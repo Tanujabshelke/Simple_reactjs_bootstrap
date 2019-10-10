4f7fafedc7baddf47925c5cf4f45216bf71e0c33
Tanuja <40301041+Tanujabshelke@users.noreply.github.com>
2019-10-11 00:38:49 +0530

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -1,30 +1,90 @@
-import React from 'react'
-import {Card} from 'react-bootstrap'
+import React, { Component } from 'react'
+import Dropzone from "react-dropzone"
 
-function Main(props) {
+class Main extends Component {
+    constructor(props) {
+                super(props)
+                this.state = {
+                               dropzone: [],
+                               topText : "",
+                               bottomText : "" ,
 
-        return(
-            <div>
-                <Card border="primary" className="m-4">
-                    <Card.Header as="h4" className=" font-weight-normal">{props.id} . {props.title}</Card.Header>
-                    <Card.Body>
-                        <Card.Title  className=" font-weight-bold">{props.subtitle}</Card.Title>
-                        <Card.Img variant="top" className="img-thumbnail my-4 w-50 h-50" src={props.img} />
-                        <Card.Title className="  font-weight-bolder">Benefits : </Card.Title>
-                        <Card.Text className="h5 font-weight-normal text-justify">
-                        {props.benefit}
-                        </Card.Text>
+                            }
+            this.handleChange = this.handleChange.bind(this)
+        }
+
+        addFilesToDropzone(imgss, dropzone) {
+    
+            imgss.map(imgs => {
+            imgs["preview"] = URL.createObjectURL(imgs);
+            
+            });
+            this.setState({ [dropzone]: imgss });
+        }
+         
+
+
+        handleChange(event){
+            const {name , value } = event.target
+            this.setState( { [name] : value })
+        }
+        render() {
+            const { dropzone } = this.state;
+            return (
+                <div>
+                    <form className="meme-form">
+                        <input type="text"
+                               placeholder="Top-Text"
+                               name="topText"
+                               value={this.state.topText}
+                               onChange={this.handleChange}
+
+                        />
+                        <input type="text"
+                               placeholder="bottom-Text"
+                               name="bottomText"
+                               value={this.state.bottomText}
+                               onChange={this.handleChange}
+
+                        />
+
+                    </form>
+                    <div className="drop">
                         
-                    </Card.Body>
-                </Card>
-            </div>
+                        <h2 className="top">{this.state.topText}</h2>
+                        <h2 className="bottom">{this.state.bottomText}</h2>
 
-        )
-    }
+                </div>
 
+                    <div >
+                        <Dropzone
+                            onDrop={imgss => {
+                                this.addFilesToDropzone(imgss, "dropzone");
+                            }}
+                            >
+                            {({ getRootProps, getInputProps }) => (
+                                <div {...getRootProps()} className="">
+                                    <input {...getInputProps()} />
+                                    <div style={{ padding: "15px",backgroundColor: "#bababa", border: "2px dashed black",textAlign:"center"}}>
+                                        Drop your image here
 
+                                        {dropzone.map(imgs => (
+                                        <img
+                                            src={imgs.preview}
+                                            alt={imgs.path}
+                                            style={{ width: "100%", height: "100%", padding : "1%" }}
+                                        />
+                                        ))}
+                                    </div>
+                                </div>
+                            )}
+                        </Dropzone>
 
-
-
-
+                
+                    </div>
+                </div>
+            
+            )
+        }
+}
 export default Main
